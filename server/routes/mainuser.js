525b5072a0289e11d926634bcfa869521f5f576f
manavshah25 <manavshah9890@gmail.com>
2023-03-12 23:16:15 +0530

    delete

--- server/routes/mainuser.js
+++
@@ -1,6 +0,0 @@
-const router=require('express').Router();
-const mainuser_controller=require('../controllers/mainuser.js')
-console.log("hello ji")
-router.post("/signup",mainuser_controller.register);
-router.post("/login",mainuser_controller.auth);
-module.exports=router;
