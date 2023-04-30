b5ebc9b7727460b87ef5b1a561058fac3808ca15
manavshah25 <manavshah9890@gmail.com>
2023-04-30 11:12:40 +0530

    Admin panel resume display

--- client/src/Component/Companyuser/index.js
+++ client/src/Component/Companyuser/index.js
@@ -10,6 +10,7 @@ function Index() {
   const [delred, setDelred] = useReducer(x => x + 1, 0)
   const [acceptred, setacceptred] = useReducer(x => x + 1, 0)
   const [rejectred, setrejectred] = useReducer(x => x + 1, 0)
+  const [viewresume, setresume] = useReducer(x => x + 1, 0)
   const nav = useNavigate()
   const jobid = localStorage.getItem("cuserid");
   useEffect(() => {
@@ -28,7 +29,7 @@ function Index() {
           console.log("error in fetching");
         }
       );
-  }, [delred, acceptred, rejectred]);
+  }, [delred, acceptred, rejectred,viewresume]);
   var accepted = "Accepted"
   var rejected = "Rejected"
   const handleclickaccept = async (userId) => {
@@ -39,6 +40,11 @@ function Index() {
 
     })
     setacceptred()
+  };
+  const handleclickresume = async (email) => {
+   localStorage.setItem("useremail",email);
+    nav("/resume")
+
   };
   const handleclickreject = async (userId) => {
 
@@ -122,10 +128,11 @@ function Index() {
                         <td>
                           <td>
                             { 
-                              <NavLink
-                                to="/resume"
+                              <a
+                               onClick={() => handleclickresume(add[0].email)}
                                 class="edit"
                                 data-toggle="modal"
+                               
                               >
                                 <svg
                                   xmlns="'http://www.w3.org/2000/svg'"
@@ -138,7 +145,7 @@ function Index() {
                                   <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
                                   <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
                                 </svg>
-                              </NavLink>
+                              </a>
                             }
                             &nbsp; &nbsp;
                             <a
